9c8ca8e2481190575fd1e6c0a5d6d00cad25d953
Nursultan <nur646132@gmail.com>
2024-08-03 17:57:54 +0500

    testing .env

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const data = [10, 12, 14, 16, 18, 20, -10];
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render('index', {
+        data: data,
+        days: days
+});
+})
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000");
+});
